233b71a959ca853e51235593ad6214dd6ed78cfc
NorIslam04 <islamaoudi18@gmail.com>
2025-06-12 01:19:46 +0100

    fix isseu send data form to email ans sheet

--- api/submit.js
+++ api/submit.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     console.log('Payload to send:', payload);
 
     // REMPLACEZ PAR VOTRE NOUVELLE URL APPS SCRIPT
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbw6SUWl7iWaFOVMdCnsfW4FAhjfaXrvuCrThmFLmRuh4Yc0oQcqSaBUVHuR3jMYe08J/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbyGZVEUY3CrzED9-ZKoR4EPZDf0kZQ5mUvmbbipBlVS_T-Gxzmi7mUQEjHa_Kt-V_jg/exec';
     
     console.log('Calling Apps Script at:', scriptUrl);
 
